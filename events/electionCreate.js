51ae2b6457743054d1adde8cca06e02d6dd72435
Arthur <ethang717163@icloud.com>
2024-07-18 01:15:25 -0500

    created another file to focus on later

---
+++ events/electionCreate.js
@@ -0,0 +1,4 @@
+/* this component will listen for the electionInitiated -- or whatever its called -- event
+when it runs, it should send a message to a dedicated channel saying an election starting
+but I am still afraid there might be errors with the election object storage so I want to worry about
+adding candidates tomorrow*/
